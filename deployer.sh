6377f38ca64a41ef6acfc258aa5ce40ceb6eff98
Juliano Mohr <jbm@redhat.com>
2022-11-17 12:47:55 +0000

    Add pass tackle service as env var

--- deployer.sh
+++ deployer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Updated /etc/passwd with current UID of the running container
 grep -v ^deployer /etc/passwd > "/tmp/passwd"
 echo "deployer:x:$(id -u):0:deployer user:/home/deployer:/bin/bash" >> /tmp/passwd
@@ -12,9 +14,12 @@ export USERNAME=deployer
 export HOME=/home/deployer
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 
+TACKLE2_SVC=${TACKLE2_SVC:-"tackle-ui"}
+
 echo "-------------------------------------------------"
 echo "Tackle 2 Setup script"
 echo "-------------------------------------------------"
+echo "Tackle2 Service:  ${TACKLE2_SVC}"
 echo "Tackle2 User:     ${TACKLE2_USER}"
 echo "Tackle2 Password: ${TACKLE2_PASSWORD}"
 echo "-------------------------------------------------"
@@ -23,6 +28,8 @@ echo "-------------------------------------------------"
 export TACKLE_NOAUTH="--no-auth"
 
 cd $HOME/tackle2-hub/hack/tool
+
+sed -i "s/REPLACE_SVC/${TACKLE2_SVC}/g" ./tackle-config.yml
 
 if [ -n "$TACKLE2_USER" ];
 then
@@ -45,7 +52,7 @@ export CURL_RC=1
 while [ "$CURL_RC" != "0" ]
 do
   sleep 5
-  curl -sI http://tackle-ui:8080 -o /dev/null
+  curl -I http://${TACKLE2_SVC}:8080
   CURL_RC=$?
   echo "  ... not yet (RC: ${CURL_RC})"
 done
