098a41d1f28da45752fd037760353ff9179366f9
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-06-28 15:48:47 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export TACKLE2_VERSION=mta-6.1
+export TACKLE2_VERSION=release-0.2
 
 podman build . --build-arg TACKLE2_VERSION=${TACKLE2_VERSION} -t quay.io/gpte-devops-automation/tackle2-setup:latest
 podman tag quay.io/gpte-devops-automation/tackle2-setup:latest quay.io/gpte-devops-automation/tackle2-setup:${TACKLE2_VERSION}
